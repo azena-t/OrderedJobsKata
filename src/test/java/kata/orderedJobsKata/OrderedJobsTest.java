766ab0e56d00c856092f025f714e14cf55a4a0e1
Kevin Timmins <kipper.timmins@googlemail.com>
2011-10-02 16:00:08 +0100

    First failing test

--- src/test/java/kata/orderedJobsKata/OrderedJobsTest.java
+++ src/test/java/kata/orderedJobsKata/OrderedJobsTest.java
@@ -7,8 +7,11 @@ import org.junit.Test;
 public class OrderedJobsTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmptyInputStringReturnsEmptyString(){
+		String expected = "";
+		String input = "";
+		String output = null;
+		assertEquals(expected,output);
 	}
 
 }
