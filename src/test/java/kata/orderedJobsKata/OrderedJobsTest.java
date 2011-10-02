5d313b07acfea62f0472934687b43ec40a559f3b
Kevin Timmins <kipper.timmins@googlemail.com>
2011-10-02 15:13:09 +0100

    Initial Config

---
+++ src/test/java/kata/orderedJobsKata/OrderedJobsTest.java
@@ -0,0 +1,14 @@
+package kata.orderedJobsKata;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderedJobsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
